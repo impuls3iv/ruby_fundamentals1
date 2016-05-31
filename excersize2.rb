d6a89a90658f3b37a4c47f68e346c277367ae233
Sam <sammy.unterman@gmail.com>
2016-05-31 13:43:49 -0400

    ex2 has 4 different excersizes in it

---
+++ excersize2.rb
@@ -0,0 +1,19 @@
+#-----TIP CALCULATOR
+meal_num = 55
+tip_num = meal_num*0.13
+puts tip_num
+#--------------
+
+#---INT TO STRING
+my_number = 10
+puts "the number i like is #{my_number.to_s}"
+
+#--------
+
+#---string interpolation
+puts "The results of multiplying 45628 by 7839 is #{45628*7839}"
+#-----
+
+#---true OR false
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#---
