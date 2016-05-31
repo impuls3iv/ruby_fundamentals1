9ba3f2cf1dcf4aa2eec144c9e5c71dd96feabe50
Sam <sammy.unterman@gmail.com>
2016-05-31 13:59:13 -0400

    ex5 modulo

---
+++ excersize4.rb
@@ -0,0 +1,16 @@
+# Write a program in a file called exercise4.rb that loops over
+# the numbers from 1 to 100. If the number is a multiple of three,
+#output the string "Bit". For multiples of five, output "Maker".
+#For numbers which are multiples of both three and five, output "BitMaker".
+#Otherwise output the number itself.
+
+(1..100).each do |i|
+  if i % 3 == 0
+    puts "Bit"
+  elsif i % 5 == 0
+    puts "Maker"
+  else
+    puts i
+  end
+end
+#.
